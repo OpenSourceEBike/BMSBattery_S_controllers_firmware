4c56b4cf5dcc714e2f3dee2a5c3e75961a4e608f
casainho <casainho@gmail.com>
2017-06-14 09:47:57 +0100

    WIP: removed not used code. ADC read is triggered after PWM pulse, just were pulse current happens. Also the motor_fast_loop() is called on the ADC interrupt.

--- gpio.c
+++ gpio.c
@@ -10,26 +10,11 @@
 #include "stm8s_gpio.h"
 #include "gpio.h"
 
-void gpio_init (void)
-{
-  ;
-}
-
-
 void brake_init (void)
 {
-  //hall sensors pins as external input pin interrupt
-//  GPIO_Init(BRAKE__PORT,
-//	    BRAKE__PIN,
-//	    GPIO_MODE_IN_FL_IT); // with external interrupt
-
   GPIO_Init(BRAKE__PORT,
 	    BRAKE__PIN,
 	    GPIO_MODE_IN_FL_NO_IT); // no interrupt
-
-  //initialize the Interrupt sensitivity
-  EXTI_SetExtIntSensitivity(EXTI_PORT_GPIOA,
-			    EXTI_SENSITIVITY_RISE_FALL);
 }
 
 BitStatus brake_is_set (void)
--- main.c
+++ main.c
@@ -51,10 +51,6 @@ uint16_t ui16_adc_value;
 // main -- start of firmware and main loop
 int main (void);
 
-//// Brake related
-// Brake signal interrupt
-void EXTI_PORTA_IRQHandler(void) __interrupt(EXTI_PORTA_IRQHANDLER);
-
 //// Hall sensors related
 // HALL SENSORS signal interrupt
 void EXTI_PORTE_IRQHandler(void) __interrupt(EXTI_PORTE_IRQHANDLER);
@@ -66,14 +62,9 @@ void motor_fast_loop (void);
 void apply_duty_cycle (uint8_t ui8_duty_cycle_value);
 
 void pwm_init (void);
-void TIM1_UPD_OVF_TRG_BRK_IRQHandler(void) __interrupt(TIM1_UPD_OVF_TRG_BRK_IRQHANDLER);
 
 // map / limit values
 int32_t map (int32_t x, int32_t in_min, int32_t in_max, int32_t out_min, int32_t out_max);
-
-void uart_init (void);
-void putchar(char c);
-char getchar(void);
 
 void adc_init (void);
 void ADC1_IRQHandler(void) __interrupt(ADC1_IRQHANDLER);
@@ -84,30 +75,7 @@ uint16_t adc_read_throttle (void);
 
 void ADC1_IRQHandler(void) __interrupt(ADC1_IRQHANDLER)
 {
-/****************************************************************
- * ADC reading and action:
- * - limitting motor max current
- * - read other ADCs for global variables
- */
-//  uint8_t ui8_temp;
-//
-//  debug_pin_set ();
-//
-//  if (adc_throttle_busy_flag == 0)
-//  {
-//      ui8_temp = ADC1->DRH;
-////     = adc_total_current;
-//
-////    if ((adc_total_current > ADC_MOTOR_TOTAL_CURRENT_MAX_POSITIVE) ||
-////	(adc_total_current < ADC_MOTOR_TOTAL_CURRENT_MAX_NEGATIVE))
-//    if (ui8_temp < ADC_MOTOR_TOTAL_CURRENT_MAX_NEGATIVE)
-//    {
-//      TIM1->BKR &= (uint8_t) ~(TIM1_BKR_MOE);
-//    }
-//  }
-//
-//  ADC1->CSR &= (uint8_t) (~ADC1_FLAG_EOC); // clear the End of Conversion flag
-///****************************************************************/
+  debug_pin_set ();
 
 /****************************************************************
  * Motor control: angle interpolation and PWM control
@@ -116,23 +84,6 @@ void ADC1_IRQHandler(void) __interrupt(ADC1_IRQHANDLER)
 /****************************************************************/
 
   ADC1_ClearITPendingBit(ADC1_IT_EOC);   // clear the interrupt pending bit
-
-//  debug_pin_reset ();
-}
-
-void TIM1_UPD_OVF_TRG_BRK_IRQHandler(void) __interrupt(TIM1_UPD_OVF_TRG_BRK_IRQHANDLER)
-{
-  debug_pin_set ();
-
-///****************************************************************
-// * Motor control: angle interpolation and PWM control
-// */
-//  motor_fast_loop ();
-///****************************************************************/
-
-
-  // clear the interrupt pending bit for TIM1
-  TIM1_ClearITPendingBit(TIM1_IT_UPDATE);
 
   debug_pin_reset ();
 }
@@ -170,7 +121,7 @@ void adc_init (void)
 	    ADC1_CHANNEL_6,
 	    ADC1_PRESSEL_FCPU_D2,
             ADC1_EXTTRIG_TIM,
-	    DISABLE,
+	    ENABLE,
 	    ADC1_ALIGN_LEFT,
 	    (ADC1_SCHMITTTRIG_CHANNEL4 || ADC1_SCHMITTTRIG_CHANNEL5 || ADC1_SCHMITTTRIG_CHANNEL6),
             DISABLE);
@@ -190,7 +141,6 @@ uint16_t adc_read_throttle (void)
 
   while (!ADC1_GetFlagStatus(ADC1_FLAG_EOC)) ; //block waiting for the end of conversion
 
-//  ADC1_GetConversionValue();
   value = ADC1->DRH;
 
   ADC1_ClearFlag(ADC1_FLAG_EOC);
@@ -219,18 +169,6 @@ void hall_sensors_read_and_action (void)
   // read hall sensors signal pins and mask other pins
   hall_sensors = (GPIO_ReadInputData (HALL_SENSORS__PORT) & (HALL_SENSORS_MASK));
 
-//  switch (hall_sensors)
-//  {
-//    case 3: hall_sensors = 1; break;
-//    case 1: hall_sensors = 5; break;
-//    case 5: hall_sensors = 4; break;
-//    case 4: hall_sensors = 6; break;
-//    case 6: hall_sensors = 2; break;
-//    case 2: hall_sensors = 3; break;
-//    default: hall_sensors = 2; return;
-//    break;
-//  }
-
   switch (hall_sensors)
   {
     case 3:
@@ -247,8 +185,6 @@ void hall_sensors_read_and_action (void)
       // count speed only when motor did rotate half of 1 electronic rotation
       if (ui8_flag_count_speed)
       {
-//	debug_pin_set ();
-
 	ui8_flag_count_speed = 0;
 	ui32_interpolation_angle_step = ui32_PWM_cycles_counter << 2; // (ui32_PWM_cycles_counter / 256) * 1024
 	ui32_PWM_cycles_counter = 0;
@@ -345,9 +281,9 @@ void apply_duty_cycle (uint8_t ui8_duty_cycle_value)
   ui8_value_c = (uint8_t) (ui16_value >> 8);
 
   // set final duty_cycle value
-  TIM1_SetCompare1((uint16_t) (ui8_value_a << 3));
-  TIM1_SetCompare2((uint16_t) (ui8_value_c << 3));
-  TIM1_SetCompare3((uint16_t) (ui8_value_b << 3));
+  TIM1_SetCompare1((uint16_t) (ui8_value_a << 2));
+  TIM1_SetCompare2((uint16_t) (ui8_value_c << 2));
+  TIM1_SetCompare3((uint16_t) (ui8_value_b << 2));
 }
 
 void pwm_init (void)
@@ -357,7 +293,7 @@ void pwm_init (void)
 
   TIM1_TimeBaseInit(0, // TIM1_Prescaler = 0
 		    TIM1_COUNTERMODE_UP,
-		    (2048 - 1), // clock = 16MHz; counter period = 1024; PWM freq = 16MHz / 1024 = 15.625kHz;
+		    (1024 - 1), // clock = 16MHz; counter period = 1024; PWM freq = 16MHz / 1024 = 15.625kHz;
 		    0); // will fire the TIM1_IT_UPDATE at every PWM period
 
   TIM1_OC1Init(TIM1_OCMODE_PWM1,
@@ -397,30 +333,14 @@ void pwm_init (void)
 		  TIM1_LOCKLEVEL_OFF,
 		  // hardware nees a dead time of 1us
 		  16, // DTG = 0; dead time in 62.5 ns steps; 1us/62.5ns = 16
-//		  TIM1_BREAK_ENABLE,
 		  TIM1_BREAK_DISABLE,
 		  TIM1_BREAKPOLARITY_LOW,
 		  TIM1_AUTOMATICOUTPUT_ENABLE);
 
-  TIM1_SelectOutputTrigger(TIM1_TRGOSource_OC1);
-
-  TIM1_ITConfig(TIM1_IT_UPDATE, ENABLE);
+  TIM1_SelectOutputTrigger(TIM1_TRGOSOURCE_UPDATE);
 
   TIM1_Cmd(ENABLE); // TIM1 counter enable
   TIM1_CtrlPWMOutputs(ENABLE); // main Output Enable
-}
-
-// Brake signal
-void EXTI_PORTA_IRQHandler(void) __interrupt(EXTI_PORTA_IRQHANDLER)
-{
-  if (brake_is_set())
-  {
-    brake_coast_disable (); // pwm main output disable
-  }
-  else
-  {
-    brake_coast_enable (); // pwm main output enable
-  }
 }
 
 // HALL SENSORS signal
@@ -441,62 +361,20 @@ void hall_sensor_init (void)
 			    EXTI_SENSITIVITY_RISE_FALL);
 }
 
-void uart_init (void)
-{
-  UART2_DeInit();
-  UART2_Init((uint32_t)115200,
-	     UART2_WORDLENGTH_8D,
-	     UART2_STOPBITS_1,
-	     UART2_PARITY_NO,
-	     UART2_SYNCMODE_CLOCK_DISABLE,
-	     UART2_MODE_TXRX_ENABLE);
-}
-
-void putchar(char c)
-{
-  //Write a character to the UART2
-  UART2_SendData8(c);
-
-  //Loop until the end of transmission
-  while (UART2_GetFlagStatus(UART2_FLAG_TXE) == RESET);
-}
-
-char getchar(void)
-{
-  uint8_t c = 0;
-
-  /* Loop until the Read data register flag is SET */
-  while (UART2_GetFlagStatus(UART2_FLAG_RXNE) == RESET) ;
-
-  c = UART2_ReceiveData8();
-
-  return (c);
-}
-
-
 int main (void)
 {
   //set clock at the max 16MHz
   CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
 
-  gpio_init ();
   brake_init ();
   while (brake_is_set()) ; // hold here while brake is pressed -- this is a protection for development
   debug_pin_init ();
-//  uart_init ();
   hall_sensor_init ();
   pwm_init ();
   adc_init ();
-//  TIM2_init ();
-
-//  GPIO_Init(GPIOD,
-//	    (GPIO_Pin_TypeDef) GPIO_PIN_0,
-//	    GPIO_MODE_OUT_PP_HIGH_FAST);
 
   ITC_SetSoftwarePriority (ITC_IRQ_ADC1, ITC_PRIORITYLEVEL_1);
-  ITC_SetSoftwarePriority (ITC_IRQ_TIM1_OVF, ITC_PRIORITYLEVEL_2);
-  ITC_SetSoftwarePriority (ITC_IRQ_PORTE, ITC_PRIORITYLEVEL_3);
-  ITC_SetSoftwarePriority (ITC_IRQ_PORTA, ITC_PRIORITYLEVEL_1);
+  ITC_SetSoftwarePriority (ITC_IRQ_PORTE, ITC_PRIORITYLEVEL_2);
 
   enableInterrupts();
 
@@ -507,11 +385,7 @@ int main (void)
 
   while (1)
   {
-//    static uint16_t c;
     static uint32_t ui32_counter = 0;
-    int8_t i8_buffer[64];
-    uint8_t ui8_value;
-    int objects_readed;
 
 //    debug_pin_reset ();
 
@@ -525,41 +399,6 @@ int main (void)
 //      ui16_adc_value = 85;
       ui8_duty_cycle = (uint8_t) map (ui16_adc_value, ADC_THROTTLE_MIN_VALUE, ADC_THROTTLE_MAX_VALUE, 0, 255);
 //      TIM1_SetCompare2((uint16_t) (85 << 2));
-//      adc_start_read_throttle ();
     }
-
-
-
-//    ui8_duty_cycle = 80;
-
-//    apply_duty_cycle (ui8_duty_cycle);
-
-//    c++;
-//    if (c < 43)
-//    {
-//      motor_fast_loop ();
-//    }
-//    else
-//    {
-//      c = 0;
-//      hall_sensors_read_and_action ();
-//    }
-
-//   fflush(stdin); // needed to unblock scanf() after a not expected formatted data
-//   objects_readed = scanf("%s %d", &i8_buffer, &ui8_value);
-//
-//   if (objects_readed > 0)
-//   {
-//     switch (i8_buffer[0])
-//     {
-//      case 'a':
-//	printf("a = %d\n", ui8_value);
-//	ui8_position_correction_value = ui8_value;
-//	break;
-//
-//      default:
-//	break;
-//     }
-//   }
   }
 }
