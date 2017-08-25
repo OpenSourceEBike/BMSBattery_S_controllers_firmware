7102fa8f4baed6a63b4fa2213fcd41d99af66ed9
stancecoke <31093785+stancecoke@users.noreply.github.com>
2017-08-25 20:56:56 +0200

    Java Tool updated
    commandline for flashing added
    function for setting optionbytes added

--- Start_Compiling.bat
+++ Start_Compiling.bat
@@ -1,5 +1,10 @@
+PATH = %PATH%C:\STMicroelectronics\st_toolset\stvp
+
 del main.hex
 make clean
 make
 ren main.ihx main.hex
+
+STVP_CmdLine -BoardName=ST-LINK -ProgMode=SWIM -Port=USB -Device=STM8S105x6 -FileProg=main.hex -verbose
+
 exit
