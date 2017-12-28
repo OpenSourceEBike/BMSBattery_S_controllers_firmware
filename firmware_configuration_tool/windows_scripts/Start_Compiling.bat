963627e73b952af1943f9b74fdca39168812494a
stancecoke <31093785+stancecoke@users.noreply.github.com>
2017-12-28 20:55:29 +0100

    path edited

--- firmware_configuration_tool/windows_scripts/Start_Compiling.bat
+++ firmware_configuration_tool/windows_scripts/Start_Compiling.bat
@@ -1,10 +1,9 @@
 PATH = %PATH%;C:\Program Files (x86)\STMicroelectronics\st_toolset\stvp
 
-del main.hex
-make -f Makefile_windows clean
-make -f Makefile_windows
-ren main.ihx main.hex
+cd ..\..
+firmware_configuration_tool\windows_scripts\make -f Makefile_windows clean
+firmware_configuration_tool\windows_scripts\make -f Makefile_windows
 
-STVP_CmdLine -BoardName=ST-LINK -ProgMode=SWIM -Port=USB -Device=STM8S105x6 -FileProg=main.hex -verbose -no_loop
+STVP_CmdLine -BoardName=ST-LINK -ProgMode=SWIM -Port=USB -Device=STM8S105x6 -FileProg=main.ihx -verbose -no_loop
 
 exit
