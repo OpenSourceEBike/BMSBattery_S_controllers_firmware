593dcac7012235d5c35c57abd0f9f883028cbe5e
stancecoke <31093785+stancecoke@users.noreply.github.com>
2017-12-28 17:36:00 +0100

    temporaribly solution to keep windows toolchain working

---
+++ Start_Compiling.bat
@@ -0,0 +1,10 @@
+PATH = %PATH%;C:\Program Files (x86)\STMicroelectronics\st_toolset\stvp
+
+del main.hex
+make -f Makefile_windows clean
+make -f Makefile_windows
+ren main.ihx main.hex
+
+STVP_CmdLine -BoardName=ST-LINK -ProgMode=SWIM -Port=USB -Device=STM8S105x6 -FileProg=main.hex -verbose -no_loop
+
+exit
---
+++ clean.bat
@@ -0,0 +1,13 @@
+cd stdperiphlib\src
+del *.asm
+del *.rel
+del *.lk
+del *.lst
+del *.rst
+del *.sym
+del *.cdb
+del *.map
+del *.elf
+del *.bin
+cd..
+cd..
