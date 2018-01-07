86171780d8de1c1f9e92f0f39c923e9f9dd9ef8c
stancecoke <31093785+stancecoke@users.noreply.github.com>
2018-01-07 12:46:28 +0100

    deleted the renaming from ihx to hex

--- firmware/Start_Compiling.bat
+++ firmware/Start_Compiling.bat
@@ -3,8 +3,7 @@ PATH = %PATH%;C:\Program Files (x86)\STMicroelectronics\st_toolset\stvp
 del main.hex
 make -f Makefile_windows clean
 make -f Makefile_windows
-ren main.ihx main.hex
 
-STVP_CmdLine -BoardName=ST-LINK -ProgMode=SWIM -Port=USB -Device=STM8S105x6 -FileProg=main.hex -verbose -no_loop
+STVP_CmdLine -BoardName=ST-LINK -ProgMode=SWIM -Port=USB -Device=STM8S105x6 -FileProg=main.ihx -verbose -no_loop
 
 exit
