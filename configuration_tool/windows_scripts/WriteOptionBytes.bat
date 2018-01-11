d1d88024928593e596e924ecabb46a055dd89b13
stancecoke <31093785+stancecoke@users.noreply.github.com>
2018-01-11 07:56:16 +0100

    fixed path problem in WriteOptionBytes.bat

--- configuration_tool/windows_scripts/WriteOptionBytes.bat
+++ configuration_tool/windows_scripts/WriteOptionBytes.bat
@@ -1,5 +1,7 @@
 PATH = %PATH%;C:\Program Files (x86)\STMicroelectronics\st_toolset\stvp
 
+cd configuration_tool\windows_scripts
+
 STVP_CmdLine -BoardName=ST-LINK -ProgMode=SWIM -Port=USB -Device=STM8S105x6 -FileOption=optionbytes.hex -verbose -no_loop
 
 exit
