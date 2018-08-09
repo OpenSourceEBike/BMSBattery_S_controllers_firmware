dbca062558719d5196205a9c46c5631f0e4870bc
stancecoke <graf.koks@freenet.de>
2018-08-09 12:39:28 +0200

    Addition of sdcc version query

--- Start_Compiling.bat
+++ Start_Compiling.bat
@@ -1,5 +1,5 @@
 PATH = %PATH%;C:\Program Files (x86)\STMicroelectronics\st_toolset\stvp;C:\SDCC\usr\local\bin;%~dp0tools\cygwin\bin
-
+sdcc --version
 del main.hex
 make -f Makefile_windows clean
 make -f Makefile_windows
