f04701bf541914194c0d22c39ca46e1417cbc62b
Bjoern Schmidt <bjoern@belix.org>
2018-10-14 16:24:32 +0200

    settings

--- config.h
+++ config.h
@@ -52,6 +52,6 @@
 #define TQS_CALIB 0.0
 #define ACA 4766
 #define EEPROM_NOINIT // eeprom will not be cleared
-#define EEPROM_INIT_MAGIC_BYTE 22 // makes sure (chance of fail 1/255) eeprom is invalidated after flashing new config
+#define EEPROM_INIT_MAGIC_BYTE 53 // makes sure (chance of fail 1/255) eeprom is invalidated after flashing new config
 
 #endif /* CONFIG_H_ */
