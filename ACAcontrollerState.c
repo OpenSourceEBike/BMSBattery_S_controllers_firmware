aa04213976511a075c7b47a267442b730cf49eb8
Björn Schmidt <bschmidt@panvision.de>
2018-10-17 10:50:09 +0200

    bugfix regen current and correction angle

--- ACAcontrollerState.c
+++ ACAcontrollerState.c
@@ -147,6 +147,7 @@ void controllerstate_init(void) {
 	ui16_battery_current_max_value = BATTERY_CURRENT_MAX_VALUE;
 	ui16_regen_current_max_value = REGEN_CURRENT_MAX_VALUE;
 	ui8_current_cal_a = current_cal_a;
+	ui8_correction_at_angle = CORRECTION_AT_ANGLE;
 	flt_torquesensorCalibration = TQS_CALIB;
 
 	// read in overrides from eeprom if they are > 0, assuming 0s are uninitialized
